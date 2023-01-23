8d45b5828bfc16f8a4d9f69d82636eb44df719e8
Trevor Ryan <me@trytig.com>
2023-01-23 14:53:13 -0500

    Update install.sh
    Removed NPM from install, Node auto installs it

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ chmod +x faxhandle.sh
 echo Installing Dependencies
 sudo apt install curl libtiff-tools -y
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash -
-sudo apt install nodejs npm -y
+sudo apt install nodejs -y
 npm install
